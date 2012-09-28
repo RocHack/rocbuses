69b7115810804c574134df45960a093a7e74643b
Steve Gattuso <stevenleeg@gmail.com>
2012-09-28 01:55:17 -0400

    Implementing JSON loaded bus data
    I may have modeled the data wrong or used a crazy way of generating
    the tables, so feel free to modify this to make it cleaner.

--- script.js
+++ script.js
@@ -4,7 +4,42 @@ $(document).ready(function() {
         e.preventDefault();
 
         var line = $(this).attr("href").replace("#", "");
-        $(".schedule").hide();
-        $("#schedule_" + line).show();
+
+        // Remove all existing schedules
+        $(".schedule").remove();
+        $.getJSON("/bus_data/"+ line +".json", getLineCallback);
     });
 });
+
+function getLineCallback(data) {
+    // Loop through each table and generate it
+    // appending it as we go.
+    $("#schedule_title").show();
+    for(var i in data) {
+        generateTable(data[i]).insertAfter("#schedule_title");
+    }
+}
+
+function generateTable(data) {
+    // Create the table
+    var table = $("<table></table>")
+        .addClass("schedule")
+
+    // Loop through destinations
+    for(var di in data['stops']) {
+        var stop = data['stops'][di];
+
+        var tr = $("<tr><td>"+stop['place']+"</tr>");
+        tr = insertTimes(tr, stop['times']);
+        table.append(tr);
+    }
+
+    return table
+}
+
+function insertTimes(tr, data) {
+    for(var i in data) {
+        tr.append("<td>" + data[i] + "</td>");
+    }
+    return tr;
+}
