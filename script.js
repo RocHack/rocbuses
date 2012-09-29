13cae88a4e52c2a9307710d03770eef399d22173
Charles Lehner <celehner1@gmail.com>
2012-09-28 22:50:46 -0400

    Add fancy scrolling,
    and have JS show/hide schedules instead of CSS

--- script.js
+++ script.js
@@ -18,15 +18,35 @@ function loadJSON(url, cb) {
 var lines = "red green orange blue silver gold".split(" ");
 
 // Update Line on hash change
+var currentScheduleEl;
 function onHashChange() {
     var line = location.hash.substr(1);
-    if (lines.indexOf(line) != -1) {
-        // let css handle showing and hiding the schedules
-        document.body.className = "line_selected line_" + line + "_selected";
-    }
+	var isLine = (lines.indexOf(line) != -1);
+
+	// hide old schedule
+	if (currentScheduleEl) {
+		resetScrolling(currentScheduleEl);
+		currentScheduleEl.style.display = "none";
+	}
+
+	// Update styles
+	document.body.className = isLine ? "line_selected" : "";
+
+    if (isLine) {
+		// Show the new schedule
+		currentScheduleEl = document.getElementById(line);
+		if (currentScheduleEl) {
+			currentScheduleEl.style.display = "block";
+			// If the table is not visible, the browser will not scroll to it.
+			// So we scroll for it.
+			currentScheduleEl.scrollIntoView();
+			initScrolling(currentScheduleEl);
+		}
+	}
 }
 onHashChange();
 window.addEventListener("hashchange", onHashChange, false);
+setTimeout(onHashChange, 10);
 
 loadJSON("schedules.json", function (redSchedule) {
     if (!redSchedule) {
@@ -42,24 +62,35 @@ function renderSchedule(line, schedule) {
     var scheduleEl = document.createElement("div");
     scheduleEl.className = "schedule";
     scheduleEl.id = line;
+    scheduleEl.addEventListener("scroll", onScheduleScroll, false);
+
     // Loop through each table and generate it
     // appending it as we go.
     for (var i = 0; i < schedule.length; i++) {
         var route = schedule[i];
-        var table = renderRoute(route, scheduleEl);
+        var tableOuter = renderRoute(route);
+		scheduleEl.appendChild(tableOuter);
     }
     return scheduleEl;
 }
 
 // Create a table for a route
-function renderRoute(data, container) {
+function renderRoute(data) {
+	var routeEl = document.createElement("div");
+	routeEl.className = "route";
+
     // Create the table
     var table = document.createElement("table");
 
     // Insert title
     var title = document.createElement("h3");
     title.appendChild(document.createTextNode(data.title));
-    container.appendChild(title);
+    routeEl.appendChild(title);
+
+    // Add another title for line wrapping purposes
+    var title2 = title.cloneNode(true);
+    title2.className = "wrapfix";
+    routeEl.appendChild(title2);
 
     // Loop through destinations
     for (var i = 0; i < data.stops.length; i++) {
@@ -76,7 +107,8 @@ function renderRoute(data, container) {
         table.appendChild(tr);
     }
 
-    container.appendChild(table);
+    routeEl.appendChild(table);
+	return routeEl;
 }
 
 function insertTimes(tr, data) {
@@ -86,3 +118,52 @@ function insertTimes(tr, data) {
         tr.appendChild(td);
     }
 }
+
+// Fancy scrolling
+
+// The THs (table headers) contain the stop names.
+// When the user scrolls horizontally through the tables,
+// shrink the THs but keep them visible and at least a certain width.
+// This helps the user see which stop each row is for.
+
+var scrollLeft,
+    maxTHWidth,
+    thWidth,
+	ths;
+
+function initTH(th) {
+	th.className = "fancyscroll";
+}
+
+// This has to to be reset when the table is hidden, otherwise 
+// the space taken up by each TH collapses.
+function resetTH(th) {
+	th.className = "";
+    th.style.width = "";
+}
+
+function updateTH(th) {
+    th.style.width = thWidth + "px";
+}
+
+function initScrolling(scheduleEl) {
+	ths = [].slice.call(scheduleEl.getElementsByTagName("th"));
+    ths.forEach(resetTH);
+	setTimeout(function () {
+		maxTHWidth = ths[0].offsetWidth - 6; // subtract padding & border
+		onScheduleScroll.call(scheduleEl);
+		ths.forEach(initTH);
+	}, 100);
+}
+
+function resetScrolling(scheduleEl) {
+	ths.forEach(resetTH);
+}
+
+function onScheduleScroll(e) {
+    var scheduleEl = this;
+    scrollLeft = scheduleEl.scrollLeft;
+    thWidth = Math.max(maxTHWidth - scrollLeft, 64);
+    ths.forEach(updateTH);
+}
+
